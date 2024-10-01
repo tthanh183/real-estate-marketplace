bc40e84c4533a28b49937e0d3669e7b865fe8087
tthanh183 <trantanthanh29@gmail.com>
2024-10-01 08:41:48 +0700

    feat: signin

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 import User from '../models/user.model.js';
 import { errorHandler } from '../utils/error.js';
@@ -10,6 +11,30 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json({ message: 'User created successfully' });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return next(errorHandler(404, 'User not found'));
+    }
+    const isPasswordValid = await bcryptjs.compare(password, user.password);
+    if (!isPasswordValid) {
+      return next(errorHandler(401, 'Wrong credentials'));
+    }
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+    const {password: pass, ...userInfo} = user._doc;
+    res
+      .cookie('access_token', token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(userInfo);
   } catch (error) {
     next(error);
   }
